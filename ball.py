9b54e54cc84fb29bc94cb281b6fec21d62fae9ac
Sudin Katuwal <sudcrack2024@gmail.com>
2026-01-25 20:31:01 -0600

    Update ball.py
    Updated speed of ball in different conditions

--- ball.py
+++ ball.py
@@ -8,20 +8,24 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.speedy_pong = 1
 
     def move(self):
-        new_x = self.xcor() + self.x_move
-        new_y = self.ycor() + self.y_move
+        new_x = self.xcor() + self.x_move * self.speedy_pong
+        new_y = self.ycor() + self.y_move * self.speedy_pong
         self.goto(new_x, new_y)
 
     def y_bounce(self):
         self.y_move *= -1
+        self.speedy_pong *= 1.05
 
     def x_bounce(self):
         self.x_move *= -1
+        self.speedy_pong *= 1.05
 
     def restart_ball(self):
         self.goto(0, 0)
         self.x_move *= -1
         self.y_move *= -1
+        self.speedy_pong = 1
         self.move()
